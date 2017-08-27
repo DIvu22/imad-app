7e7fa13d6fda0c2ff27a07455f3060dc0f04c4db
DIvu22 <divu1666@gmail.com>
2017-08-27 13:50:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,13 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.click=function()
 {
     var request= new XMLHttpRequest();
     request.onreadystatechange=function()
     {
-        if(request.readyState---XMLHttpRequest.DONE)
+        if(request.readyState == XMLHttpRequest.DONE)
         {
-          if(request.status==200)
+          if(request.status == 200)
           {
              var counter= request.responseText;
               var span=document.getElementById('count');
