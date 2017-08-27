40f4c1faf6706d69e4c3e7c4662bcab8252e8626
DIvu22 <divu1666@gmail.com>
2017-08-27 12:07:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,9 @@ app.get('/article-one',function(req,res){
     
 
 });
+ app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
 
 
 app.get('/ui/main.js', function (req, res) {
