37379940255df2b9d55cb458099c19afe689585e
DIvu22 <divu1666@gmail.com>
2017-09-16 12:39:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,10 @@
 var button=document.getElementById("counter");
 
-button.click=function()
+button.onclick=function()
 {
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function()
-    {
-        if(request.readyState == XMLHttpRequest.DONE)
-        {
-          if(request.status == 200)
-          {
-             var counter= request.responseText;
-              var span=document.getElementById("count");
-              span.innerHTML=counter.toString();
-          }
-        }
-        
-    };
-   
-   
-   //render the variable
-request.open('GET','http://divu1666.imad.hasura-app.io/counter',true);
-request.send(null);
-   
+    var counter=0;
     
+    counter= counter=1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
