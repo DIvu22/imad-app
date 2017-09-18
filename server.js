a1f5f2cdde8a8de22da286f3462264cc604e03e6
DIvu22 <divu1666@gmail.com>
2017-09-18 19:43:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
-var crypto=require('crypto');
+var crypto = require('crypto');
 
 var config={
     user:'divu1666',
@@ -120,7 +120,7 @@ function hash(input,salt)
 
 app.get('/hash/:input',function(req,res)
 {
-    var hashedString=hash(req.params.inpu,'this is some random string');
+    var hashedString=hash(req.params.input,'this is some random string');
     res.send(hashedString);
 });
 
